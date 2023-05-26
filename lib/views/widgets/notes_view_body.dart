6f25cc21c550c916802dc29bf2a4206b7f4f2037
Mostafa Rabie Alaasr <73349514+MostafaEla3sr@users.noreply.github.com>
2023-05-26 11:32:37 +0200

    create notes cubit

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'package:notes_app/views/widgets/notes_list_view.dart';
 
@@ -10,16 +8,13 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: BlocProvider(
-        create: (context) => NotesCubit(),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          child: Column(
-            children: const [
-              CustomAppBar(title: 'Notes', icon: Icons.search,),
-              Expanded(child: NotesListView()),
-            ],
-          ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24 , vertical: 16),
+        child: Column(
+          children: const [
+            CustomAppBar(title: 'Notes' , icon: Icons.search,),
+            Expanded(child: NotesListView()),
+          ],
         ),
       ),
     );
